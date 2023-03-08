b9d841021972dc912ab9573b3210dc17daedf1f5
Te <worksingletonmcdaniel@gmail.com>
2023-03-07 21:33:45 -0800

    added automation for folder/subfolder organization year>month

--- mv_screenshots.sh
+++ mv_screenshots.sh
@@ -10,3 +10,16 @@ dest_dir="$HOME/Desktop/screenshots"
 mv "$src_dir/Screen"*.png "$dest_dir/"
 mv "$src_dir/Screen"*.png "$dest_dir/"
 mv "$src_dir/Screen"*.png "$dest_dir/"
+
+#organize screenshots into folders by date.
+
+cd "$dest_dir"
+for file in *.png *.jpg *.jpeg; do
+    if [ -f "$file" ]; then
+        date=$(stat -f "%Sm" -t "%Y-%m-%d" "$file")
+        year=$(echo "$date" | cut -d'-' -f1)
+        month=$(echo "$date" | cut -d'-' -f2)
+        mkdir -p "$dest_dir/$year/$month"
+        mv "$file" "$dest_dir/$year/$month/"
+    fi
+done
